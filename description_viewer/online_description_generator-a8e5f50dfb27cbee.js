5c4f14a327d0e5c0525b564e5682d5ca908c0eb1
matthiasbeyer <mail@beyermatthias.de>
2023-07-24 06:00:50 +0000

    Deploying to gh-pages from @ matthiasbeyer/type_description@fafc6eb055c00a1db5f8f7a706bc44d214719317 🚀

--- description_viewer/online_description_generator-a8e5f50dfb27cbee.js
+++ description_viewer/online_description_generator-a8e5f50dfb27cbee.js
@@ -200,7 +200,7 @@ function makeMutClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_12(arg0, arg1, arg2) {
-    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc27a8a0a57d8b5c7(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5add4a0951c624fb(arg0, arg1, addHeapObject(arg2));
 }
 
 function isLikeNone(x) {
@@ -388,8 +388,8 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper147 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 54, __wbg_adapter_12);
+    imports.wbg.__wbindgen_closure_wrapper145 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 51, __wbg_adapter_12);
         return addHeapObject(ret);
     };
 
@@ -426,7 +426,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('online_description_generator-8c537580f6cf06ac_bg.wasm', import.meta.url);
+        input = new URL('online_description_generator-a8e5f50dfb27cbee_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
